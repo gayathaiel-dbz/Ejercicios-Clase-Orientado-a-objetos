1f20ee807cc205e0511b17b1ca1604beef9c53a2
Thaiel Gaya <gayathaiel@gmail.com>
2026-08-31 22:24:52 -0300

    errores

--- Ejercicios/Clase2_Ejercicio_tarea.py
+++ Ejercicios/Clase2_Ejercicio_tarea.py
@@ -1,39 +1,41 @@
 class Paciente:
-    def _init_(self, nombre, dni):
+    def __init__(self, nombre, dni):
         self.nombre = nombre
         self.dni = dni
-        
-    def arregla_texto(devuelve):
-        return f"{devuelve.nombre} (DNI: {devuelve.dni})"
+
+    def arregla_texto(self):
+        return f"{self.nombre} (DNI: {self.dni})"
+
 
 class Turnos:
-    def _init_(self, paciente, fecha, hora):
+    def __init__(self, paciente, fecha, hora):
         self.paciente = paciente
         self.fecha = fecha
         self.hora = hora
-    
-    def arregla_texto(devuelve):
-        return f"Fecha : {devuelve.fecha} | Hora: {devuelve.hora} | Paciente: {devuelve.paciente}"
-    
-class Agenda: 
-    def _init_(self):
-        self.turnos
-        
+
+    def arregla_texto(self):
+        return f"Fecha: {self.fecha} | Hora: {self.hora} | Paciente: {self.paciente.arregla_texto()}"
+
+
+class Agenda:
+    def __init__(self):
+        self.turnos = []
+
     def agregar_turno(self, nuevo_turno):
         for t in self.turnos:
-            if t.fecha == nuevo_turno.fecha and t.hora == nuevo_turno.hora: 
-                print(f" Error: El horario {nuevo_turno.fecha} a las {nuevo_turno.hora}")
-                return 
-        
+            if t.fecha == nuevo_turno.fecha and t.hora == nuevo_turno.hora:
+                print(f"Error: El horario {nuevo_turno.fecha} a las {nuevo_turno.hora} ya está ocupado.")
+                return
+
         self.turnos.append(nuevo_turno)
-        print(f"Turno agendado con exito para {nuevo_turno.paciente.nombre}")
-    
+        print(f"Turno agendado con éxito para {nuevo_turno.paciente.nombre}")
+
     def listar_turnos(self):
         print("Turnos registrados en la agenda")
+
         if len(self.turnos) == 0:
             print("No hay turnos")
             return
-        
+
         for t in self.turnos:
             print(t.arregla_texto())
-            
