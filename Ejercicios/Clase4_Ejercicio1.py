dec03ef70ac98dd138ea8093f33dced4022c638b
Thaiel Gaya <gayathaiel@gmail.com>
2026-09-16 08:37:59 -0300

    falta probar

--- Ejercicios/Clase4_Ejercicio1.py
+++ Ejercicios/Clase4_Ejercicio1.py
@@ -1,14 +1,29 @@
+from abc import ABC, abstractmethod
 class Personajes(ABC):
-    def __init__(self,nombre):
+    def __init__(self, nombre):
         self.nombre = nombre
     
     @abstractmethod
     def atacar(self):
         pass
     
-class Guerrero(Personaje):
+class Guerrero(Personajes):
     def __init__(self,nombre):
         super().__init__(nombre)
     
     def atacar(self):
         print(f"{self.nombre} (Guerrero) ataca con su arma")
+
+class Mago(Personajes):
+    def __init__(self, nombre):
+        super().__init__(nombre)
+    
+    def atacar(self):
+        print(f"{self.nombre} (Mago) ataca con hechizo")
+
+class Arquero(Personajes):
+    def __init__(self, nombre):
+        super().__init__(nombre)
+    
+    def atacar(self):
+        print(f"{self.nombre} (Arquero) ataca con flecha")
