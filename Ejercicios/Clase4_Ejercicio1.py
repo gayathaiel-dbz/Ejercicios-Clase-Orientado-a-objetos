ae69529aec4d1b96b164432a872d38759bf0daf4
Thaiel Gaya <gayathaiel@gmail.com>
2026-09-16 08:25:10 -0300

    progreso

--- Ejercicios/Clase4_Ejercicio1.py
+++ Ejercicios/Clase4_Ejercicio1.py
@@ -4,3 +4,11 @@ class Personajes(ABC):
     
     @abstractmethod
     def atacar(self):
+        pass
+    
+class Guerrero(Personaje):
+    def __init__(self,nombre):
+        super().__init__(nombre)
+    
+    def atacar(self):
+        print(f"{self.nombre} (Guerrero) ataca con su arma")
